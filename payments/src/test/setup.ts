d02c49ba62c188080496b695712f315a77ae8317
AbdulKreemShah2408 <abdulkreemshah92@gmail.com>
2026-08-06 20:44:03 +0500

    Add missing test setup file

--- payments/src/test/setup.ts
+++ payments/src/test/setup.ts
@@ -3,16 +3,18 @@ import mongoose from 'mongoose';
 import jwt from 'jsonwebtoken';
 
 declare global {
-  var signin: () => string[];
+  var signin: (id?: string) => string[];
 }
+
 jest.mock('../nats-wrapper');
 
 let mongo: any;
+
 beforeAll(async () => {
   process.env.JWT_KEY = 'asdfasdf';
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-  const mongo = await MongoMemoryServer.create();
+  mongo = await MongoMemoryServer.create();
   const mongoUri = mongo.getUri();
 
   await mongoose.connect(mongoUri, {});
@@ -36,25 +38,19 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = () => {
-  // Build a JWT payload.  { id, email }
+global.signin = (id?: string) => {
   const payload = {
-    id: new mongoose.Types.ObjectId().toHexString(),
+    id: id || new mongoose.Types.ObjectId().toHexString(),
     email: 'test@test.com',
   };
 
-  // Create the JWT!
   const token = jwt.sign(payload, process.env.JWT_KEY!);
 
-  // Build session Object. { jwt: MY_JWT }
   const session = { jwt: token };
 
-  // Turn that session into JSON
   const sessionJSON = JSON.stringify(session);
 
-  // Take JSON and encode it as base64
   const base64 = Buffer.from(sessionJSON).toString('base64');
 
-  // return a string thats the cookie with the encoded data
   return [`session=${base64}`];
 };
